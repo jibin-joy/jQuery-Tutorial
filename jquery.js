6a19e96bb7df6c95c1896f8f8cf409c54275f332
Jibin-Sample <jibinkjoy3101@gmail.com>
2023-11-09 23:56:05 +0530

    Jquery-Tuorial

---
+++ jquery.js
@@ -0,0 +1,10 @@
+// jQuery("h1").css("color", "red");
+$("h1").css("color", "orange");
+
+$("button").click(function () {
+  $("h1").hide();
+});
+
+$("button").click(function () {
+  $("button").css("color", "red");
+});
